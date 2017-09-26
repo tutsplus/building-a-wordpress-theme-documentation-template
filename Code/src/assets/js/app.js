e7d49db3c602dfbd8c12e815e6e1a9c562f9a66b
Ian Yates <ian@snaptin.com>
2017-09-26 09:34:24 +0200

    init commit

---
+++ Code/src/assets/js/app.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+import whatInput from 'what-input';
+
+window.$ = $;
+
+// import Foundation from 'foundation-sites';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+import './lib/foundation-explicit-pieces';
+
+
+$(document).foundation();
+
+import Prism from './lib/prism';
